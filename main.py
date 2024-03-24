ea31b5d7b60ca5a93c9d884957dbbb3d2d6a7d33
Mitchell Shibilski-Unkel <133730558+MitchellShibilski-Unkel@users.noreply.github.com>
2024-03-24 10:11:56 -0600

    Improved Graphics
    - Adds an improved looking navbar
    - *Disclaimer: Find is NOT functional, yet

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtGui import *
 
 
 class MNet(QMainWindow):
-    def __init__(self, url: str, caption: str):
+    def __init__(self, url: str, caption: str, width: int, height: int):
         super(MNet, self).__init__()
         self.browser = QWebEngineView()
         
         # Set the title of the window
         self.setWindowTitle(caption)
+        
+        # Set default window size
+        self.resize(width, height)
         
         # Set default search engine
         self.browser.setUrl(QUrl(url))
@@ -22,6 +26,8 @@ class MNet(QMainWindow):
         
         # Nav
         self.navbar = QToolBar()
+        self.navbar.setFont(QFont("Hopper", 10))
+        self.navbar.setStyleSheet("background-color: white; color: gray;"), self.navbar.setStyle(QStyleFactory.create("Fusion"))
         self.addToolBar(self.navbar)
         self.__addButton__("←", "b"), self.__addButton__("→", "f"), self.__addButton__("⟳", "r")
         
@@ -29,6 +35,8 @@ class MNet(QMainWindow):
         self.urlText.returnPressed.connect(self.openURL)
         self.navbar.addWidget(self.urlText)
         self.browser.urlChanged.connect(self.updateURL)
+        
+        self.__find__()
 
     def openURL(self):
         url = self.urlText.text()
@@ -36,6 +44,14 @@ class MNet(QMainWindow):
 
     def updateURL(self, q):
         self.urlText.setText(q.toString())
+        
+    def navBarCanMove(self, can: bool):
+        self.navbar.setMovable(can)
+        
+    def __find__(self):
+        findBar = QLineEdit()
+        findBar.setPlaceholderText("Find...")
+        self.navbar.addWidget(findBar)
         
     def __addButton__(self, icon: str, action: str):
         btn = QAction(icon, self)
@@ -49,5 +65,6 @@ class MNet(QMainWindow):
         self.navbar.addAction(btn)
         
 app = QApplication(sys.argv)
-window = MNet('https://duckduckgo.com/?kae=-1&ks=t&kau=-1&kak=-1&kax=-1&km=m', "MNet")
+window = MNet('https://duckduckgo.com/?kae=-1&ks=t&kau=-1&kak=-1&kax=-1&km=m', "MNet", 500, 500)
+window.navBarCanMove(False)
 app.exec()
